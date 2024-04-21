de24400318ec8be225dbb528ceb9bc2a7258aae9
andres <a20191450@pucp.edu.pe>
2024-04-21 14:09:00 -0500

    estructura proyecto

--- README.md
+++
@@ -1,2 +0,0 @@
-# PROYECTO-GTICS-2024-1
- Proyecto del curso gtics, creacion de una plataforma de una cadena de farmacias
---
+++ webapp/src/main/java/com/example/webapp/controller/superadmin/SuperadminController.java
@@ -0,0 +1,91 @@
+package com.example.webapp.controller.superadmin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/superadmin")
+public class SuperadminController{
+
+    @GetMapping("")
+    public String Plantilla() {
+        return "superadmin/Plantilla";
+    }
+    @GetMapping("/Actualizar_Administrador")
+    public String Actualizar_Administrador() {
+        return "superadmin/Plantilla_Vista_Actualizar_Administrador";
+    }
+    @GetMapping("/Actualizar_Doctor")
+    public String Actualizar_Doctor() {
+        return "superadmin/Plantilla_Vista_Actualizar_Doctor";
+    }
+    @GetMapping("/Actualizar_Farmacista")
+    public String Actualizar_Farmacista() {
+        return "superadmin/Plantilla_Vista_Actualizar_Farmacista";
+    }
+    @GetMapping("/Actualizar_Medicamento")
+    public String Actualizar_Medicamento() {
+        return "superadmin/Plantilla_Vista_Actualizar_Medicamento";
+    }
+    @GetMapping("/Actualizar_Paciente")
+    public String Actualizar_Paciente() {
+        return "superadmin/Plantilla_Vista_Actualizar_Paciente";
+    }
+    @GetMapping("/Estado_Envios")
+    public String Estado_Envios() {
+        return "superadmin/Plantilla_Vista_Estado_Envios";
+    }
+    @GetMapping("/Estado_Solicitudes_Farmacistas")
+    public String Estado_Solicitudes_Farmacistas() {
+        return "superadmin/Plantilla_Vista_Estado_Solicitudes_Farmacistas";
+    }
+    @GetMapping("/Medicamentos")
+    public String Medicamentos() {
+        return "superadmin/Plantilla_Vista_Medicamentos";
+    }
+    @GetMapping("/Vista_Principal")
+    public String Vista_Principal() {
+        return "superadmin/Plantilla_Vista_Principal";
+    }
+    @GetMapping("/Registrar_Medicamento")
+    public String Registrar_Medicamento() {
+        return "superadmin/Plantilla_Vista_Registrar_Medicamento";
+    }
+    @GetMapping("/Registro_Administrador")
+    public String Registro_Administrador() {
+        return "superadmin/Plantilla_Vista_Registro_Administrador";
+    }
+    @GetMapping("/Registro_Doctor")
+    public String Registro_Doctor() {
+        return "superadmin/Plantilla_Vista_Registro_Doctor";
+    }
+    @GetMapping("/Ver_Administrador")
+    public String Ver_Administrador() {
+        return "superadmin/Plantilla_Vista_Ver_Administrador";
+    }
+    @GetMapping("/Ver_Doctor")
+    public String Ver_Doctor() {
+        return "superadmin/Plantilla_Vista_Ver_Doctor";
+    }
+    @GetMapping("/Ver_Farmacista")
+    public String Ver_Farmacista() {
+        return "superadmin/Plantilla_Vista_Ver_Farmacista";
+    }
+    @GetMapping("/Ver_Medicamento")
+    public String Ver_Medicamento() {
+        return "superadmin/Plantilla_Vista_Ver_Medicamento";
+    }
+    @GetMapping("/Ver_Paciente")
+    public String Ver_Paciente() {
+        return "superadmin/Plantilla_Vista_Ver_Paciente";
+    }
+
+}
+
