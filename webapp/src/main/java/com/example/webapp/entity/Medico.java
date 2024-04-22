db55f1457fd696bd52156bf348298ef59c3366bb
andres <a20191450@pucp.edu.pe>
2024-04-21 23:33:07 -0500

    enlazada a la base de datos

--- webapp/src/main/java/com/example/webapp/entity/Medico.java
+++ webapp/src/main/java/com/example/webapp/entity/Medico.java
@@ -7,15 +7,15 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name="paciente")
-public class Paciente {
+@Table(name="medico")
+public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_paciente")
-    private int idpaciente;
+    @Column(name="id_medico")
+    private int idmedico;
     @Column(nullable = false)
-    private String seguro;
-    private String edad;
-    private int usuario_id_usuario;
-    private String telefono;
+    private String nombre;
+    private String apellido;
+    private int sede_id_sede;
+    private String colegiatura_medico;
 }
