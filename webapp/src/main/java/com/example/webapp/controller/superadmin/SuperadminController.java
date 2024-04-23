feffcd46fd2673747c514dcd578dc3fe5e4bdfd6
Julio Cesar <a20203521@pucp.edu.pe>
2024-04-23 09:24:25 -0500

    commit 3 superadmin

--- webapp/src/main/java/com/example/webapp/controller/superadmin/SuperadminController.java
+++ webapp/src/main/java/com/example/webapp/controller/superadmin/SuperadminController.java
@@ -1,4 +1,4 @@
-package com.example.webapp.entity.superadmin;
+package com.example.webapp.controller.superadmin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
