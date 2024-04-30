c7917e51d46641c3d8702ce60820ca3eb12d4dce
Julio Cesar <a20203521@pucp.edu.pe>
2024-04-29 21:58:46 -0500

    cruds usuarios 5 superadmin

--- webapp/src/main/java/com/example/webapp/controller/superadmin/SuperadminController.java
+++ webapp/src/main/java/com/example/webapp/controller/superadmin/SuperadminController.java
@@ -23,74 +23,48 @@ public class SuperadminController {
     UsuarioRepository usuarioRepository;
 
     public SuperadminController(MedicamentosRepository medicamentosRepository,
-                                UsuarioRepository usuarioRepository){
+                                UsuarioRepository usuarioRepository) {
 
         this.medicamentosRepository = medicamentosRepository;
 
         this.usuarioRepository = usuarioRepository;
     }
 
-
     @GetMapping("")
     public String Plantilla() {
         return "superadmin/Plantilla";
     }
-    @GetMapping("/Actualizar_Administrador")
-    public String Actualizar_Administrador() {
-        return "superadmin/Plantilla_Vista_Actualizar_Administrador";
-    }
-    @GetMapping("/Actualizar_Doctor")
-    public String Actualizar_Doctor() {
-        return "superadmin/Plantilla_Vista_Actualizar_Doctor";
-    }
-    @GetMapping("/Actualizar_Farmacista")
-    public String Actualizar_Farmacista() {
-        return "superadmin/Plantilla_Vista_Actualizar_Farmacista";
-    }
-    @GetMapping("/Actualizar_Medicamento")
-    public String Actualizar_Medicamento() {
-        return "superadmin/Plantilla_Vista_Actualizar_Medicamento";
-    }
-    @GetMapping("/Actualizar_Paciente")
-    public String Actualizar_Paciente() {
-        return "superadmin/Plantilla_Vista_Actualizar_Paciente";
-    }
+
     @GetMapping("/Estado_Envios")
     public String Estado_Envios() {
         return "superadmin/Plantilla_Vista_Estado_Envios";
     }
+
     @GetMapping("/Estado_Solicitudes_Farmacistas")
     public String Estado_Solicitudes_Farmacistas() {
         return "superadmin/Plantilla_Vista_Estado_Solicitudes_Farmacistas";
     }
+
     @GetMapping("/Registrar_Medicamento")
     public String Registrar_Medicamento() {
         return "superadmin/Plantilla_Vista_Registrar_Medicamento";
     }
+
     @GetMapping("/Registro_Administrador")
     public String Registro_Administrador() {
         return "superadmin/Plantilla_Vista_Registro_Administrador";
     }
+
     @GetMapping("/Registro_Doctor")
     public String Registro_Doctor() {
         return "superadmin/Plantilla_Vista_Registro_Doctor";
     }
-    @GetMapping("/Ver_Administrador")
-    public String Ver_Administrador() {
-        return "superadmin/Plantilla_Vista_Ver_Administrador";
-    }
-    @GetMapping("/Ver_Farmacista")
-    public String Ver_Farmacista() {
-        return "superadmin/Plantilla_Vista_Ver_Farmacista";
-    }
-    @GetMapping("/Ver_Paciente")
-    public String Ver_Paciente() {
-        return "superadmin/Plantilla_Vista_Ver_Paciente";
-    }
+
     @GetMapping("/Ver_Perfil")
     public String Ver_Perfil() {
         return "superadmin/Perfil";
     }
+
     @GetMapping("/Cerrar_Cuenta")
     public String Cerrar_Cuenta() {
         return "superadmin/Index";
@@ -100,9 +74,9 @@ public class SuperadminController {
     //------------------------------------------------------------------------------------------------------------------
     //Listar Medicamentos
     @GetMapping("/Medicamentos")
-    public String Medicamentos(Model model){
+    public String Medicamentos(Model model) {
         List<Medicamentos> lista = medicamentosRepository.findAll();
-        model.addAttribute("listTransportation",lista);
+        model.addAttribute("listTransportation", lista);
         return "superadmin/Plantilla_Vista_Medicamentos";
     }
 
@@ -134,7 +108,7 @@ public class SuperadminController {
     //Editar Medicamento
     @GetMapping("/Editar_Medicamento")
     public String editarMedicamento(Model model,
-                                      @RequestParam("id") int id) {
+                                    @RequestParam("id") int id) {
 
         Optional<Medicamentos> optMedicamento = medicamentosRepository.findById(id);
         if (optMedicamento.isPresent()) {
@@ -169,21 +143,40 @@ public class SuperadminController {
 
     //Listar Usuarios
     @GetMapping("/Vista_Principal")
-    public String Usuarios(Model model){
+    public String Usuarios(Model model) {
         List<Usuario> lista = usuarioRepository.findByRol("Doctor");
-        model.addAttribute("listTransportation",lista);
+        model.addAttribute("listTransportation", lista);
         List<Usuario> lista1 = usuarioRepository.findByRol("Administrador");
-        model.addAttribute("listTransportation1",lista1);
+        model.addAttribute("listTransportation1", lista1);
         List<Usuario> lista2 = usuarioRepository.findByRol("Farmacista");
-        model.addAttribute("listTransportation2",lista2);
+        model.addAttribute("listTransportation2", lista2);
         List<Usuario> lista3 = usuarioRepository.findByRol("Paciente");
-        model.addAttribute("listTransportation3",lista3);
+        model.addAttribute("listTransportation3", lista3);
         return "superadmin/Plantilla_Vista_Principal";
     }
 
+    @PostMapping("/Guardar_Usuario")
+    public String guardar_Doctor(Usuario usuario) {
+        usuarioRepository.save(usuario);
+        return "redirect:/superadmin/Vista_Principal";
+    }
+
+    @GetMapping("/Eliminar_Usuario")
+    public String borrarDoctor(@RequestParam("id") int id) {
+
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+
+        if (optUsuario.isPresent()) {
+            usuarioRepository.deleteById(id);
+        }
+        return "redirect:/superadmin/Vista_Principal";
+
+    }
+
+    //CRUD DOCTOR
     @GetMapping("/Ver_Doctor")
     public String Ver_Doctor(Model model,
-                                  @RequestParam("id") int id) {
+                             @RequestParam("id") int id) {
         Optional<Usuario> optUsuario = usuarioRepository.findById(id);
 
         if (optUsuario.isPresent()) {
@@ -197,7 +190,7 @@ public class SuperadminController {
 
     @GetMapping("/Editar_Doctor")
     public String editar_Doctor(Model model,
-                                    @RequestParam("id") int id) {
+                                @RequestParam("id") int id) {
 
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
         if (optionalUsuario.isPresent()) {
@@ -209,10 +202,91 @@ public class SuperadminController {
         }
     }
 
-    @PostMapping("/Guardar_Doctor")
-    public String guardar_Doctor(Usuario usuario) {
-        usuarioRepository.save(usuario);
-        return "redirect:/superadmin/Vista_Principal";
+
+    //CRUD ADMINISTRADOR
+    @GetMapping("/Ver_Administrador")
+    public String Ver_Administrador(Model model,
+                                    @RequestParam("id") int id) {
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+        if (optUsuario.isPresent()) {
+            Usuario usuario = optUsuario.get();
+            model.addAttribute("usuario", usuario);
+            return "superadmin/Plantilla_Vista_Ver_Administrador";
+        } else {
+            return "redirect:superadmin/Plantilla_Vista_Principal";
+        }
     }
+
+    @GetMapping("/Editar_Administrador")
+    public String editar_Administrador(Model model,
+                                       @RequestParam("id") int id) {
+
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            model.addAttribute("usuario", usuario);
+            return "superadmin/Plantilla_Vista_Actualizar_Administrador";
+        } else {
+            return "redirect:/superadmin/Plantilla_Vista_Principal";
+        }
+    }
+
+    //CRUD FARMACISTA
+    @GetMapping("/Ver_Farmacista")
+    public String Ver_Farmacista(Model model,
+                                 @RequestParam("id") int id) {
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+        if (optUsuario.isPresent()) {
+            Usuario usuario = optUsuario.get();
+            model.addAttribute("usuario", usuario);
+            return "superadmin/Plantilla_Vista_Ver_Farmacista";
+        } else {
+            return "redirect:superadmin/Plantilla_Vista_Principal";
+        }
+    }
+
+    @GetMapping("/Editar_Farmacista")
+    public String editar_Farmacista(Model model,
+                                    @RequestParam("id") int id) {
+
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            model.addAttribute("usuario", usuario);
+            return "superadmin/Plantilla_Vista_Actualizar_Farmacista";
+        } else {
+            return "redirect:/superadmin/Plantilla_Vista_Principal";
+        }
+    }
+
+    //CRUD PACIENTE
+    @GetMapping("/Ver_Paciente")
+    public String Ver_Paciente(Model model,
+                                 @RequestParam("id") int id) {
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+        if (optUsuario.isPresent()) {
+            Usuario usuario = optUsuario.get();
+            model.addAttribute("usuario", usuario);
+            return "superadmin/Plantilla_Vista_Ver_Paciente";
+        } else {
+            return "redirect:superadmin/Plantilla_Vista_Principal";
+        }
+    }
+
+    @GetMapping("/Editar_Paciente")
+    public String editar_Paciente(Model model,
+                                    @RequestParam("id") int id) {
+
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            model.addAttribute("usuario", usuario);
+            return "superadmin/Plantilla_Vista_Actualizar_Paciente";
+        } else {
+            return "redirect:/superadmin/Plantilla_Vista_Principal";
+        }
+    }
+
+
 }
 
