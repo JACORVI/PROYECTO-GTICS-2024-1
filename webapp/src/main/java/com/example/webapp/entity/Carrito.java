4561d6eaa022b5316a33e5ac46a55ecf72094d57
andres <a20191450@pucp.edu.pe>
2024-05-06 15:39:44 -0500

    avance carrito

--- webapp/src/main/java/com/example/webapp/entity/Carrito.java
+++ webapp/src/main/java/com/example/webapp/entity/Carrito.java
@@ -9,10 +9,20 @@ import lombok.Setter;
 @Setter
 @Table(name="carrito")
 public class Carrito {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="carrito_id")
-    private int id;
+
+    @EmbeddedId
+    private CarritoId id;
+
+    @MapsId("usuario_id_usuario")
+    @ManyToOne
+    @JoinColumn(name="usuario_id_usuario")
+    private Usuario usuario_id_usario;
+
+    @MapsId("medicamentos_id_medicamentos")
+    @ManyToOne
+    @JoinColumn(name="medicamentos_id_medicamentos")
+    private Medicamentos medicamentos_id_medicamentos;
+
     @Column(nullable = false)
     private int cantidad;
 }
