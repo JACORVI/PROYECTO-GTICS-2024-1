08a5e5a751cd55b9b0a3c26e240b9768b41e0a33
7elvis <a20192663@pucp.edu.pe>
2024-05-06 19:11:39 -0500

    perfil

--- webapp/src/main/java/com/example/webapp/controller/farmacista/FarmacistaController.java
+++ webapp/src/main/java/com/example/webapp/controller/farmacista/FarmacistaController.java
@@ -1,4 +1,29 @@
 package com.example.webapp.controller.farmacista;
 
+import com.example.webapp.entity.*;
+import com.example.webapp.repository.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Controller
+
+@RequestMapping("/farmacista")
 public class FarmacistaController {
+    @GetMapping("/farmacista/nuevopedido")
+    public String Pedidos(Model model){
+
+        return "farmacista/nuevo_pedido";
+    }
+    @GetMapping("/farmacista/perfil")
+    public String Perfil(Model model){
+
+        return "farmacista/perfil";
+    }
 }
