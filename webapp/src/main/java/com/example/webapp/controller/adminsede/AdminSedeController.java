50941b231b21bdec171bc936fc1cc6b58467e46b
JACORVI <117043793+JACORVI@users.noreply.github.com>
2024-05-06 22:00:29 -0500

    lo hizo damian
    gaaa

--- webapp/src/main/java/com/example/webapp/controller/adminsede/AdminSedeController.java
+++ webapp/src/main/java/com/example/webapp/controller/adminsede/AdminSedeController.java
@@ -1,20 +1,71 @@
 package com.example.webapp.controller.adminsede;
 
+import com.example.webapp.repository.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminSedeController {
-    @GetMapping(value = "/adminsede/inicio")
+    final
+    MedicamentosRepository medicamentosRepository;
+    UsuarioRepository usuarioRepository;
+    PedidosReposicionRepository pedidosReposicionRepository;
+    UsuarioHasSedeRepository usuarioHasSedeRepository;
+    SedeRepository sedeRepository;
+    SedeHasMedicamentosRepository sedeHasMedicamentosRepository;
+
+    public AdminSedeController(MedicamentosRepository medicamentosRepository,
+                                UsuarioRepository usuarioRepository,
+                                PedidosReposicionRepository pedidosReposicionRepository,
+                                UsuarioHasSedeRepository usuarioHasSedeRepository,
+                                SedeRepository sedeRepository,
+                                SedeHasMedicamentosRepository sedeHasMedicamentosRepository) {
+
+        this.medicamentosRepository = medicamentosRepository;
+
+        this.usuarioRepository = usuarioRepository;
+
+        this.pedidosReposicionRepository = pedidosReposicionRepository;
+
+        this.usuarioHasSedeRepository = usuarioHasSedeRepository;
+
+        this.sedeRepository = sedeRepository;
+
+        this.sedeHasMedicamentosRepository = sedeHasMedicamentosRepository;
+    }
+
+
+    @GetMapping(value = "/admin/inicioSupAdmin")
     public String adminsedeinicio(){
-        return "admin/Vista_Principal_Admin";
+        return "admin/inicioSupAdmin";
     }
-    @GetMapping(value ="/adminsede/reposición" )
+    @GetMapping(value ="/admin/pedidos_reposicion" )
     public String adminsedereposicion(){
-        return "admin/Vista_Pedidos_de_Reposición";
+        return "admin/pedidos_reposicion";
     }
-    @GetMapping(value="/adminsede/medicamentos")
-    public String adminsedemedciamentos(){
-        return "admin/Vista_Medicamentos_Admin";
+    @GetMapping(value="/admin/medicamentosSupAdmin")
+    public String adminsedemedciamentos(Model model){
+        /*List<Medicamentos> listMed = medicamentosRepository.findAll();
+        model.addAttribute("listMed",listMed );*/
+        return "admin/medicamentosSupAdmin";
+    }
+
+    @GetMapping(value="/admins/nuevo_pedido")
+    public String adminsedePedidos(){
+
+        return "admin/nuevo_pedido";
+    }
+
+    @GetMapping(value="/admin/farmacistas")
+    public String adminsedeFarmacistas(){
+
+        return "admin/farmacistas";
+    }
+
+    @GetMapping(value="/admin/estado_soliciutd_farmacistas")
+    public String adminsedeSolFarmacistas(){
+
+        return "admin/estado_soliciutd_farmacistas";
     }
 }
