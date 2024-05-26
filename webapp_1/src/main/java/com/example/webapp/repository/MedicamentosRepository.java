9dbe77f26aa54cfc3a1a1f8ef64f8a2169af63a7
7elvis <a20192663@pucp.edu.pe>
2024-05-26 00:03:16 -0500

    dto-repository-entidades actualizadas

--- webapp_1/src/main/java/com/example/webapp/repository/MedicamentosRepository.java
+++ webapp_1/src/main/java/com/example/webapp/repository/MedicamentosRepository.java
@@ -1,8 +1,9 @@
 package com.example.webapp.repository;
 
+import com.example.webapp.dto.MedicamentoSolicitados3mesesDto;
+import com.example.webapp.dto.MedicamentoSolicitadosxdiasDto;
 import com.example.webapp.dto.MedicamentosPocoInventarioDto;
 import com.example.webapp.entity.Medicamentos;
-import com.example.webapp.entity.Usuario;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -33,4 +34,17 @@ public interface MedicamentosRepository extends JpaRepository<Medicamentos, Inte
 
     @Query(value = "select nombre as Medicamento, inventario as Inventario from medicamentos m inner join sede_has_medicamentos shm  on (m.id_medicamentos = shm.medicamentos_id_medicamentos) where sede_id_sede = ?1 and (inventario < 25)", nativeQuery = true)
     List<MedicamentosPocoInventarioDto> medicamentosConPocoInventario(int sede_id_sede);
+
+    @Query(value = "SELECT m.nombre as Medicamento, count(m.nombre) as CantidadSolicitada FROM medicamentos m inner join pedidos_reposicion_has_medicamentos prhm on (m.id_medicamentos = prhm.medicamentos_id_medicamentos) \n" +
+            "inner join pedidos_reposicion pr on (prhm.pedidos_reposicion_id_pedidos_reposicion = pr.id_pedidos_reposicion) where pr.fecha_solicitud >= date_sub(curdate(), INTERVAL ?1 DAY) \n" +
+            "group by m.nombre order by cantidadSolicitada desc limit 3;", nativeQuery = true)
+    List<MedicamentoSolicitadosxdiasDto> medicamentosSolicitadosxdias(int cantDias);
+
+    @Query(value = "SELECT m.nombre as Medicamento, count(m.nombre) as CantidadSolicitada FROM medicamentos m inner join pedidos_reposicion_has_medicamentos prhm on (m.id_medicamentos = prhm.medicamentos_id_medicamentos) \n" +
+            "inner join pedidos_reposicion pr on (prhm.pedidos_reposicion_id_pedidos_reposicion = pr.id_pedidos_reposicion) where pr.fecha_solicitud >= date_sub(curdate(), INTERVAL 3 MONTH) \n" +
+            "group by m.nombre order by cantidadSolicitada desc limit 3;", nativeQuery = true)
+    List<MedicamentoSolicitados3mesesDto> medicamentosSolicitados3meses();
+
+    @Query(value = "select * from sede_has_medicamentos shm inner join medicamentos m on (shm.medicamentos_id_medicamentos = m.id_medicamentos) where sede_id_sede = ?1 and (inventario < 25)", nativeQuery = true)
+    List<Medicamentos> listarMedicamentosConPocoInvporSede(int sede_id_sede);
 }
