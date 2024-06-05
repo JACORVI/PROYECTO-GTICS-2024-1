b01a722d9901956e5196c5d1aa85f646b68677bc
7elvis <a20192663@pucp.edu.pe>
2024-06-04 20:51:28 -0500

    cuerpo: activar y recuperar cambiado- falta el cuerpo registrar admin/doctor

--- webapp/src/main/java/com/example/webapp/util/Correo.java
+++ webapp/src/main/java/com/example/webapp/util/Correo.java
@@ -80,7 +80,7 @@ public class Correo {
                 + "</head>\n"
                 + "<body>\n"
                 + "    <center>\n"
-                + "        <div style='width: 600px; text-align: left; color: #333; font-family: Arial, sans-serif;'>\n"
+                + "        <div style='width: 600px; text-align: left; color: #333; font-family: Arial, sans-serif; font-size: 16px;'>\n"
                 + "            <div style='background-color: #0f99ab; padding: 20px;'>\n"
                 + "                <img src='https://www.example.com/logo.png' alt='Logo' style='width: 100px; height: auto;' />\n"
                 + "                <h1 style='color: #fff; font-size: 24px;'>REESTABLECER CONTRASEÑA: PildoPharm</h1>\n"
@@ -120,36 +120,30 @@ public class Correo {
                 + "</head>\n"
                 + "<body>\n"
                 + "    <center>\n"
-                + "        <div style='text-align: left; color: rgba(15, 153, 171, 1); width: 670px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);transition: 0.3s'>\n"
-                + "            <div style='padding: 2px 16px;'>\n"
-                + "                <div style='background:  rgba(15, 153, 171, 1) !important; padding: 10px; '>\n"
-                + "                    <table>\n"
-                + "                        <tr>\n"
-                + "                            <td>\n"
-                + "                                <span style='font-size: 24px; \n"
-                + "                                font-family:LucidaGrande,tahoma,verdana,arial,sans-serif;\n"
-                + "                                color: white;'>ACTIVAR CUENTA: PildoPharm</span>\n"
-                + "                            </td>\n"
-                + "                        </tr>\n"
-                + "                    </table>\n"
-                + "                </div>\n"
-                + "       \n"
+                + "        <div style='width: 600px; text-align: left; color: #333; font-family: Arial, sans-serif; font-size: 16px;'>\n"
+                + "            <div style='background-color: #0f99ab; padding: 20px;'>\n"
+                + "                <img src='https://www.example.com/logo.png' alt='Logo' style='width: 100px; height: auto;' />\n"
+                + "                <h1 style='color: #fff; font-size: 24px;'>ACTIVAR CUENTA: PildoPharm</h1>\n"
+                + "            </div>\n"
+                + "            <div style='padding: 20px; background-color: #f4f4f4;'>\n"
                 + "                <p>Hola, @@_NOMBRES_COMPLETOS_@@:</p>\n"
-                + "                <span style='text-align: justify;'>Recibimos una solicitud para activar tu cuenta.<br />"
-                + "  Ingresa al url proporcionado para su activación:</span><br />\n"
-                + "                </span>\n"
-                + "              <p style='font-weight: bold;'>Link: <a href='@@_URL_@@' target='_blank'>@@_URL_@@</a></p> \n"
-                + "                 <p style='font-style: italic;'>Nota: Recuerda que este url solo es válido por por única vez. </p>\n"
+                + "                <p>Recibimos una solicitud para activar tu cuenta.<br />\n"
+                + "                Ingresa al siguiente link proporcionado para su activación:</p>\n"
+                + "                <p style='text-align: center; margin: 20px 0;'>\n"
+                + "                    <a href='@@_URL_@@' style='background-color: #0f99ab; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px;'>Link de activación</a>\n"
+                + "                </p>\n"
+                + "                <p style='font-style: italic;'>Nota: Recuerda que este url solo es válido por una única vez.</p>\n"
                 + "                <p>Bienvenido a la familia PildoPharm.</p>\n"
                 + "            </div>\n"
                 + "        </div>\n"
                 + "    </center>\n"
                 + "</body>\n"
-                + "</html>;";
+                + "</html>";
 
-        cuerpo = cuerpo.replaceAll("@@_URL_@@", "" + url);
+        cuerpo = cuerpo.replaceAll("@@_URL_@@", url);
         cuerpo = cuerpo.replaceAll("@@_NOMBRES_COMPLETOS_@@", obj.getNombres() + " " + obj.getApellidos());
 
         return cuerpo;
     }
+
 }
