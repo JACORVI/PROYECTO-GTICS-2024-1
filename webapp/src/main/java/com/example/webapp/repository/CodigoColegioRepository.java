333f51087abd975148f74c8cfdf45a7af10c50c0
andres <a20191450@pucp.edu.pe>
2024-07-09 00:40:16 -0500

    arreglando

--- webapp/src/main/java/com/example/webapp/repository/CodigoColegioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.webapp.repository;
-
-import com.example.webapp.entity.CodigoColegio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CodigoColegioRepository extends JpaRepository<CodigoColegio, Integer> {
-}
--- webapp/src/main/java/com/example/webapp/repository/DistritoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.webapp.repository;
-
-import com.example.webapp.entity.Distrito;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DistritoRepository extends JpaRepository<Distrito, Integer> {
-}
